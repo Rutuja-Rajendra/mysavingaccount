e0ab466266fde29f40f7d85571ddaa7e1e4746c8
Rutuja-Rajendra <rutub175@gmail.com>
2026-07-02 00:55:50 +0530

    initial commit

---
+++ src/main/java/com/mysavingaccount/app/MysavingaccountApplication.java
@@ -0,0 +1,13 @@
+package com.mysavingaccount.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysavingaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysavingaccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysavingaccount/app/MysavingaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysavingaccount.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysavingaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
