8bd63fa6ea9566b7ca66d8e70618f475233f4d8c
Rutuja-Rajendra <rutub175@gmail.com>
2026-07-26 00:31:10 +0530

    authentication is working fine, started coding savingplans logic

--- src/main/java/com/mysavingaccount/app/controller/SavingPlanController.java
+++ src/main/java/com/mysavingaccount/app/controller/SavingPlanController.java
@@ -1,5 +1,29 @@
 package com.mysavingaccount.app.controller;
 
-public class SavingPlanController {
+import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mysavingaccount.app.entity.SavingPlan;
+import com.mysavingaccount.app.service.SavingPlanService;
+
+@RestController
+@RequestMapping("/api/savings")
+public class SavingPlanController {
+	
+	@Autowired
+	private SavingPlanService savingPlanService;
+
+	@PostMapping("/start")
+	public SavingPlan createSavingPlan(@RequestBody BigDecimal amount)
+	{
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();	
+		
+		return savingPlanService.createSavingPlan(email, amount);
+	}
 }
--- src/main/java/com/mysavingaccount/app/service/SavingPlanService.java
+++ src/main/java/com/mysavingaccount/app/service/SavingPlanService.java
@@ -1,5 +1,46 @@
 package com.mysavingaccount.app.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mysavingaccount.app.entity.SavingPlan;
+import com.mysavingaccount.app.entity.User;
+import com.mysavingaccount.app.repository.SavingPlanRepository;
+import com.mysavingaccount.app.repository.UserRepository;
+
+@Service
 public class SavingPlanService {
 
+	
+	@Autowired
+	private SavingPlanRepository savingPlanRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public SavingPlan createSavingPlan(String userEmail, BigDecimal amount)
+	{
+		User user = userRepository.findByEmail(userEmail);
+	    if (user == null) {
+	        throw new RuntimeException("User not found");
+	    }
+	    
+		SavingPlan savingPlan = new SavingPlan();
+		
+		LocalDate nextDueDate = LocalDate.now().plusMonths(1);
+		
+		savingPlan.setUser(user);
+		savingPlan.setAmount(amount);
+		savingPlan.setFrequency("MONTHLY");
+		savingPlan.setStatus("ACTIVE");
+		savingPlan.setNextDueDate(nextDueDate);
+		savingPlan.setCreatedAt(LocalDateTime.now());
+		
+		return savingPlanRepository.save(savingPlan);
+	}
 }
